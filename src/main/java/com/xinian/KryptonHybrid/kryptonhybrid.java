a5d9c31c0cfddf14b371d98882dc4ec02faa19f6
dreamforSh <3856889129@qq.com>
2026-03-27 21:07:13 +0800

    init

---
+++ src/main/java/com/xinian/KryptonHybrid/kryptonhybrid.java
@@ -0,0 +1,13 @@
+package com.xinian.KryptonHybrid;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(kryptonhybrid.MODID)
+public class kryptonhybrid {
+    public static final String MODID = "krypton_hybrid";
+
+    public kryptonhybrid(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
